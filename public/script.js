a470334df511fd80a160308668ad71baf704f1cc
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-21 14:14:23 +0530

    done with the shopping cart

--- public/script.js
+++ public/script.js
@@ -10,8 +10,43 @@ new Vue({
 		cart: [],
 	},
 	methods: {
-		addToCart() {
-			this.total += 9.99;
+		addToCart: function (product) {
+			this.total += product.price;
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					qty: 1,
+				});
+			}
+		},
+		inc: function (item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function (item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i, 1);
+			}
+		},
+		onSubmit: function () {
+			console.log("Search");
+		},
+	},
+	filters: {
+		currency: function (price) {
+			return "$".concat(price.toFixed(2));
 		},
 	},
 });
